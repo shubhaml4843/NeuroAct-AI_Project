22f0a596f799cb9a02ab620cb2da0f02e8365ede
Shubham Lokare <shubhamlokare4843@gmail.com>
2025-08-05 23:56:23 +0530

    chnage in requirements.txt

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 """Main entrypoint for NeuroAct AI."""
 import uvicorn
-from app.interface.fastapi_main import app
 
 if __name__ == "__main__":
     print("🚀 Starting NeuroAct AI Server...")
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.interface.fastapi_main:app", host="0.0.0.0", port=8000, reload=True)
